c5a2319dab64990b29a25b8b0fb3a3cc07c2248a
JAYALAXMIMADISI <jayalaxmi.madisi@narayanagroup.com>
2023-04-04 18:06:23 +0530

    Story book practice

--- src/Project/index.js
+++ src/Project/index.js
@@ -2,11 +2,12 @@ import { Primary } from "@storybook/addon-docs";
 import React from "react";
 import { Secondary, Success } from "../components/Button/Button.stories";
 import Input from "../components/Input/Input";
+import { Small } from "../components/Input/Input.stories";
 
 export default function Home() {
   return <>
   <Secondary />
   <Success/>
-  <Input/>
+  <Small/>
   </>;
 }
